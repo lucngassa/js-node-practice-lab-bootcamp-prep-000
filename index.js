ac06aadc4b6de73d5b8761df8d0a0193e001897d
Luc Ngassa <ngassaluc@gmail.com>
2019-10-18 21:57:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 // write your code below!
 
-var name = "Luc"
-var height = "75"
-
-console.log("Name:")
-console.log(name)
-
-console.log("Height:")
-console.log(parseInt(height) +1)
+var height = 74;
